4a3eb650f603e01d5840543b301742a52b69f406
Sandeep Samra <ssamra67@gmail.com>
2016-01-11 17:19:44 -0500

    main menu and user input section complete

--- contact_list.rb
+++ contact_list.rb
@@ -4,5 +4,28 @@ require_relative 'contact'
 class ContactList
 
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+  puts "What would you like to do?"
+  command = gets.chomp
+
+  case command
+  when "new"
+    puts "What is the contact's name?"
+    name = gets.chomp
+    puts "What is the contact's email?"
+    email = gets.chomp
+    Contact.create(name, email)
+  when "list"
+    Contact.all
+  when "show"
+    puts "What is the contact ID?"
+    id = gets.chomp
+    Contact.find(id)
+  when "search"
+    puts "Please enter a search term"
+    term = gets.chomp
+    Contact.search(term)
+  else
+    puts "Here is a list of available commands:", "new - Create a new contact", "list - List all contacts", "show - Show a contact", "search - Search contacts"
+  end
 
 end
